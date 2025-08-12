4e0c7f9aef6c2479e2705b01f905d7080a09c368
Robert <barrosor@oregonstate.edu>
2025-08-11 20:22:55 -0400

    Merge exisitng repo with local project

--- old/promiseExample.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs").promises;
-
-fs.readFile("example.txt", "utf8")
-  .then((text1) => {
-    console.log(text1);
-  })
-  .catch((err) => {
-    console.log("Error: ", err);
-  });
--- old/writeAndRead.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const exampleText = "This is a test file for Node.js";
-
-fs.writeFileSync("testfile.txt", exampleText);
-
-const finalRead = fs.readFileSync("testfile.txt", "utf8");
-console.log(finalRead);
